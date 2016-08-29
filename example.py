be67f0cd6d8fd96394d4041e12936b07c1b4458f
Dmitry V Selitsky <OR>
2016-08-29 16:59:01 +0300

    pep8; py3k compat; requirements

--- example.py
+++ example.py
@@ -1,22 +1,26 @@
 import random
 
 from gevent import monkey
+
 monkey.patch_all()
 
 from flask import Flask
 from flask_beanstalk import Beanstalk
 
-
 app = Flask(__name__)
+app.config['BEANSTALK_HOST'] = '127.0.0.1'
+app.config['BEANSTALK_PORT'] = 11300
+
 beanstalk = Beanstalk(app)
 
 
 @app.route('/')
 def index():
-  secs = random.randint(0, 10)
-  beanstalk.put(str(secs))
-  return "placed job that sleeps for %d seconds" % secs
+    secs = random.randint(0, 10)
+    beanstalk.use('twitter')
+    beanstalk.put(str(secs))
+    return "placed job that sleeps for %d seconds" % secs
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
